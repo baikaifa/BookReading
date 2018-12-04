d2c2a3f55fd59152d9d9d8fe798d18dca84e23e4
李元彪 <274008566@qq.com>
2018-12-04 10:42:51 +0800

    Update app.module.ts

--- people/孙晓雪/app.module.ts
+++ people/孙晓雪/app.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
