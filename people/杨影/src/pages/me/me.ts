fd47ebc12dae038a9b7eafb8d8aa5a5f3988868d
yangying1998 <1204749635@qq.com>
2018-12-04 08:11:26 +0800

    心阅

---
+++ people/杨影/src/pages/me/me.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { QianbaoPage} from '../qianbao/qianbao'
+import { XiaoxiPage} from '../xiaoxi/xiaoxi'
+/**
+ * Generated class for the MePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-me',
+  templateUrl: 'me.html',
+})
+export class MePage {
+public LoginPage=LoginPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+qianbao(){
+  this.navCtrl.push(QianbaoPage);
+}
+xiaoxi(){
+  this.navCtrl.push(XiaoxiPage);
+}
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MePage');
+  }
+}
---
+++ people/杨影/src/pages/qianbao/qianbao.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MePage} from '../me/me'
+/**
+ * Generated class for the QianbaoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-qianbao',
+  templateUrl: 'qianbao.html',
+})
+export class QianbaoPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  me(){
+    this.navCtrl.pop();
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad QianbaoPage');
+  }
+
+}
