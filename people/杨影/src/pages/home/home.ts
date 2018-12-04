63b7a072cc4e92a10a1ca30e3a6e164b977dccfd
yangying1998 <1204749635@qq.com>
2018-12-04 16:04:44 +0800

    心阅

--- people/杨影/src/pages/home/home.ts
+++ people/杨影/src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { HttpClient } from '@angular/common/http';
+import { QiandaoPage } from '../../shujia/qiandao/qiandao';
+import { SousuoPage } from '../../shujia/sousuo/sousuo';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private http:HttpClient,public navCtrl: NavController) {
+    this.http.get('/api').subscribe((data)=>{
+      console.log(data);
+    })
+  
+  }
+  qiandao(){
+    this.navCtrl.push(QiandaoPage);
+  }
+  sousuo(){
+    this.navCtrl.push(SousuoPage);
   }
 
 }
