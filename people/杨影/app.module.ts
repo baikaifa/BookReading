2a9b99c822d5f5cf492d0d023a935c0428da829c
yangying1998 <1204749635@qq.com>
2018-12-04 08:34:20 +0800

    心阅

---
+++ people/杨影/app.module.ts
@@ -0,0 +1,66 @@
+
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+//自定义组件
+import { AboutPage } from '../pages/about/about';
+
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { MePage } from '../pages/me/me';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ConfigProvider } from '../providers/config/config';
+import { HttpServicesProvider } from '../providers/http-services/http-services';
+import { StorageProvider } from '../providers/storage/storage';
+import { RegistersignPage } from '../pages/registersign/registersign';
+// 我的钱包页面
+import { QianbaoPage } from '../pages/qianbao/qianbao';
+//我的消息页面
+import { XiaoxiPage } from '../pages/xiaoxi/xiaoxi';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    MePage,
+    HomePage,
+    TabsPage,
+    LoginPage,
+    RegisterPage,
+    RegistersignPage,
+    QianbaoPage,
+    XiaoxiPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp,{
+      tabsHideOnSubPages:'true'
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    MePage,
+    HomePage,
+    TabsPage,
+    LoginPage,
+    RegisterPage,
+    RegistersignPage,
+    QianbaoPage,
+    XiaoxiPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ConfigProvider,
+    HttpServicesProvider,
+    StorageProvider
+  ]
+})
+export class AppModule {}
