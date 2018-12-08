bedf84a2dac665f3d4e8263ecf978707ab63e835
李元彪 <274008566@qq.com>
2018-12-08 13:13:38 +0800

    12.8 微调

---
+++ people/李元彪/xinyue1.3.1/src/pages/originate/yuanchaungtuijian/yuanchaungtuijian.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the YuanchaungtuijianPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-yuanchaungtuijian',
+  templateUrl: 'yuanchaungtuijian.html',
+})
+export class YuanchaungtuijianPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad YuanchaungtuijianPage');
+  }
+
+}
