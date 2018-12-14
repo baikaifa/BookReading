f0102166fe95b1a53a8afdda00575b2b86944073
sunxiaoxue0304 <597232104@qq.com>
2018-12-14 09:49:24 +0800

    1.1

---
+++ people/孙晓雪/1/qiantai/src/pages/bookstore/read8/read8.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Read8Page } from './read8';
+
+@NgModule({
+  declarations: [
+    Read8Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Read8Page),
+  ],
+})
+export class Read8PageModule {}
